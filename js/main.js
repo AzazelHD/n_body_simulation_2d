40bc9b358bf6a6c7d44c0afa365d3aad36203b9a
Aza <diegogym@live.com>
2024-12-14 17:54:28 +0100

    added basic simulation controls

--- js/main.js
+++ js/main.js
@@ -6,22 +6,6 @@ const $$ = (selector) => document.querySelectorAll(selector);
 
 const canvas = $("#canvas");
 
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Simulation parameters
-const aspectRatio = canvas.width / canvas.height;
-const camera = new THREE.OrthographicCamera(
-  -aspectRatio * 500,
-  aspectRatio * 500,
-  500,
-  -500,
-  1,
-  1000
-);
-camera.position.z = 10;
-
 const pos1 = new THREE.Vector2(0, 0);
 const pos2 = new THREE.Vector2(0, -200);
 const pos3 = new THREE.Vector2(0, 200);
@@ -30,16 +14,62 @@ const vel1 = new THREE.Vector2(0, 0);
 const vel2 = new THREE.Vector2(-5, 0);
 const vel3 = new THREE.Vector2(2, 0);
 
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Celestial Bodies initialization
 const bodies = [
   new CelestialBody(pos1, vel1, 4000, "white", scene),
   new CelestialBody(pos2, vel2, 100, "red", scene),
   // new CelestialBody(pos3, vel3, 500, "teal", scene),
 ];
 
-let steps = 5;
+let zoom = 1;
+
+// Camera setup
+const aspectRatio = canvas.width / canvas.height;
+const camera = new THREE.OrthographicCamera(
+  (-aspectRatio * 500) / zoom, // Left
+  (aspectRatio * 500) / zoom, // Right
+  500 / zoom, // Top
+  -500 / zoom, // Bottom
+  1, // Near
+  1000 // Far
+);
+
+camera.position.set(...pos1, 100);
+
+const dtInput = $("#dtInput");
+const dtValue = $("#dtValue");
+const stepsInput = $("#stepsInput");
+const stepsValue = $("#stepsValue");
+
+// Simulation parameters
+let steps = 1;
 let dt = 1;
 let G = 1;
 
+dtInput.value = dt;
+dtValue.textContent = dt;
+stepsInput.value = steps;
+stepsValue.textContent = steps;
+
+dtInput.addEventListener("input", function () {
+  dt = dtInput.value;
+  dtValue.textContent = dt;
+  cancelAnimationFrame(simulationId);
+  simulate(dt, steps, G);
+});
+
+stepsInput.addEventListener("input", function () {
+  steps = stepsInput.value;
+  stepsValue.textContent = steps;
+  cancelAnimationFrame(simulationId);
+  simulate(dt, steps, G);
+});
+
+let simulationId;
 function simulate(dt, steps = 1, G = 1) {
   for (let i = 0; i < steps; i++) {
     // Apply gravitational forces
@@ -57,8 +87,10 @@ function simulate(dt, steps = 1, G = 1) {
     });
   }
 
+  camera.position.set(...bodies[0].position, 100);
+
   renderer.render(scene, camera);
-  requestAnimationFrame(() => simulate(dt, steps, G));
+  simulationId = requestAnimationFrame(() => simulate(dt, steps, G));
 }
 
 document.body.onkeydown = function (e) {
